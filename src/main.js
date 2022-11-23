1290886d3a1b4d5f8320592c7d0fe5db48a86089
wzt <2238091763@qq.com>
2022-11-23 11:36:14 +0800

    [update]infra

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import bus from './bus'
+import router from "@/router/index";
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.config.globalProperties.$bus = bus
+
+app.mount('#app')
