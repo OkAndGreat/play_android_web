462bd148a4f4712650f1abce1902be70afd8dc4a
OkAndGreat <2238091763@qq.com>
2023-05-12 22:41:48 +0800

    完成UserOpt

--- src/components/UserOpt.vue
+++ src/components/UserOpt.vue
@@ -4,8 +4,14 @@
 
 <template>
     <div class="userOpt">
-
+        <a class="btn" href="/api">本站开放API</a>
+        <a class="btn" href="/api">待办清单</a>
+        <a class="btn" href="/api">分享文章</a>
+        <a class="btn" href="/api">分享项目</a>
+        <a class="btn new" href="/api">本站APP下载</a>
+        <a class="btn new" href="/api">问答征集</a>
     </div>
+
 </template>
 
 <style scoped>
@@ -16,7 +22,44 @@
     padding-bottom: 10px;
     background-color: white;
     margin-top: 20px;
-    box-shadow: 0 1px 2px 0 rgba(0,0,0,.05);
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, .05);
     border-radius: 3px;
+
+    .btn {
+        height: 40px;
+        background-color: #3498db;
+        cursor: pointer;
+        text-align: center;
+        border-radius: 3px;
+        margin-left: 10px;
+        margin-top: 10px;
+        color: #fff;
+        display: inline-block;
+        position: relative;
+    }
+
+    .new::before {
+        /**
+        这里通过
+                position: absolute;
+        top: -3px;
+        left: -2px;
+        定位了new 旗帜
+        并且通过position: absolute; 让其显性
+         */
+        position: absolute;
+        top: -4px;
+        left: -2px;
+        z-index: 1;
+        line-height: 0px;
+        font-size: 12px;
+        color: #fff;
+        height: 0px;
+        border: 7px solid #e64c3b;
+        border-right-color: transparent;
+        border-radius: 3px;
+        content: "New";
+        box-shadow: 0px 0.3125rem 0.3125rem -0.3125rem #565656;
+    }
 }
 </style>
