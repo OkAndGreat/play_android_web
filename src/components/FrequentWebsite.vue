6e85b83ec91eb6c6d31815d42ac3a4158134cdef
OkAndGreat <2238091763@qq.com>
2023-05-14 22:09:31 +0800

    完成常用网站

--- src/components/FrequentWebsite.vue
+++ src/components/FrequentWebsite.vue
@@ -1,7 +1,6 @@
 <script>
 /* eslint-disable */
 import {getFrequentWebsite} from "@/network/API";
-import {List} from "vant";
 
 const TAG = "FrequentWebsite"
 export default {
@@ -9,7 +8,8 @@ export default {
     data() {
         return {
             hotKeyList: [],
-            hotKeyMap: new Map()
+            hotKeyMap: new Map(),
+            newHotKeyList: []
         }
     },
     created() {
@@ -22,6 +22,9 @@ export default {
             this.hotKeyList = res.data
             console.log(TAG + "[hotkeys:] --> " + JSON.stringify(this.hotKeyList))
         },
+        mapToArray(map) {
+            return Array.from(map)
+        }
     },
     watch: {
         hotKeyList(newValue) {
@@ -33,6 +36,8 @@ export default {
                     this.hotKeyMap.set(item.category, [item])
                 }
             }
+            this.newHotKeyList = Array.from(this.hotKeyMap.values()).map(list => [...list])
+            console.log(TAG + "[newHotKeyList:] --> " + JSON.stringify(this.newHotKeyList))
         }
     }
 }
@@ -41,6 +46,10 @@ export default {
 <template>
     <div class="FrequentWebsite">
         <h2 class="title">常用网站</h2>
+        <div v-for="value in newHotKeyList" class="singleCategory">
+            <a class="itemCategoryFirst">{{ value[0].category + ':' }}</a>
+            <a v-for="category in value" class="singleCategoryItem">{{ category.name }}</a>
+        </div>
     </div>
 </template>
 
@@ -48,6 +57,8 @@ export default {
 .FrequentWebsite {
   margin-top: 20px;
   background-color: white;
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, .05);
+  border-radius: 3px;
 
   .title {
     height: 40px;
@@ -58,7 +69,47 @@ export default {
     color: #333;
     text-indent: 10px;
     white-space: nowrap;
+    margin-bottom: 0;
+  }
 
+  .singleCategory {
+    padding: 5px 10px 5px 10px;
+    border-top: 1px dashed #e2e2e2;
+
+    a {
+      margin: 3px 8px 3px 8px;
+      text-decoration: none;
+      font-size: 14px;
+      text-align: center;
+
+      &:hover {
+        color: inherit;
+      }
+    }
+
+
+    .itemCategoryFirst {
+      background-color: #1b93fb;
+      padding-right: 5px;
+      padding-left: 5px;
+      color: #ffffff !important;
+    }
+
+    .singleCategoryItem {
+      display: inline-block;
+      cursor: pointer;
+      color: #767676;
+
+      &:hover {
+        color: #1b93fb;
+        text-decoration: underline;
+      }
+    }
+
+    &:first-of-type {
+      border-top: none;
+    }
   }
 }
 </style>
+
